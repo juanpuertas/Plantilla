710e1eeee65a6464a31c2c9e2c831ce6fe381ae0
Juan Puertas <110198316+juanpuertas@users.noreply.github.com>
2023-08-07 16:53:50 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Components/Header'
+
+const App = () => {
+  
+  
+  return (
+    <div >
+        <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      light: '#757ce8',
+      main: '#ff9800',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}> 
+      <CssBaseline/>
+      <App/>
+    </ThemeProvider>
+  </React.StrictMode>,
+)
